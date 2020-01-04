8f9015519e43dca1dec72b15c16bc32fef7b1344
Mary Gibbs <mgibbs1259@gmail.com>
2020-01-04 15:18:16 -0500

    initial commit

---
+++ email_significant_other.py
@@ -0,0 +1,34 @@
+import logging
+import smtplib, ssl
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+import credentials
+
+
+def email_significant_other():
+    """Send an email to the significant other."""
+    # Set up the email
+    message = MIMEMultipart("alternative")
+    message["From"] = credentials.sender_email_credentials["sender_email_address"]
+    message["To"] = credentials.recipient_email_credentials["recipient_email_address"]
+    message["Subject"] = "Weekly Reminders"
+    with open("weekly_reminders.txt", "r") as weekly_reminders:
+        text = weekly_reminders.read()
+    message_text = MIMEText(text, "plain")
+    message.attach(message_text)
+    # Send the email
+    try:
+        context = ssl.create_default_context()
+        with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+            server.login(message["From"], credentials.sender_email_credentials["sender_email_password"])
+            server.sendmail(message["From"], message["To"], message.as_string())
+    except Exception as e:
+        logger.error(e, exc_info=True)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(filename="/tmp/email_signigicant_other.log", filemode="w", level=logging.ERROR,
+                        format="%(asctime)s %(levelname)s %(message)s")
+    logger = logging.getLogger(__name__)
+    email_significant_other()
---
+++ example_credentials.py
@@ -0,0 +1,11 @@
+# Sender email credentials
+sender_email_credentials = {
+    "sender_email_address": "SENDER EMAIL ADDRESS",
+    "sender_email_password": "SENDER EMAIL PASSWORD"
+}
+
+
+# Recipient email credentials
+recipient_email_credentials = {
+    "recipient_email_address": "RECIPIENT EMAIL ADDRESS"
+}
